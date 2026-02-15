128f5c30be1c4afec53fa2b3941d33c55ba1ec5f
Ahmad Almadi <ahmadalmadi2005@gmail.com>
2026-02-16 02:03:14 +0300

    lightning-bg

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,19 +1,21 @@
-import GridScan from './bg/GridScan';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import AboutUs from './components/AboutUs';
-import Services from './components/Services';
-import Portfolio from './components/Portfolio';
-import Team from './components/Team';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import GridScan from "./bg/GridScan";
+import Lightning from "./bg/Lightning";
+
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import AboutUs from "./components/AboutUs";
+import Services from "./components/Services";
+import Portfolio from "./components/Portfolio";
+import Team from "./components/Team";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import "./global.css";
 
 function App() {
   return (
-    <div style={{ position: 'relative', width: '100%', minHeight: '100vh' }}>
+    <div style={{ position: "relative", width: "100%", minHeight: "100vh" }}>
       {/* GridScan Background - Fixed position */}
-      <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: 0 }}>
+      {/* <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: 0 }}>
         <GridScan
           sensitivity={0.55}
           lineThickness={1}
@@ -26,8 +28,11 @@ function App() {
           chromaticAberration={0.002}
           noiseIntensity={0.01}
         />
+      </div> */}
+      <div style={{ width: "100%", height: "100vh", position: "fixed" }}>
+        <Lightning hue={260} xOffset={0} speed={1} intensity={1} size={1} />
       </div>
-      
+
       {/* Content - Above background */}
       <div className="content-wrapper">
         <Navbar />
