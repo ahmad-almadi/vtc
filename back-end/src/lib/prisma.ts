4019d27ec36c117de43616659789a3956061ea80
Ahmad Almadi <ahmadalmadi2005@gmail.com>
2026-03-13 23:59:27 +0300

    fix

--- back-end/src/lib/prisma.ts
+++ back-end/src/lib/prisma.ts
@@ -1,9 +1,7 @@
 import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
-import pg from 'pg';
-
-const { Pool } = pg;
+import { Pool } from 'pg';
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
